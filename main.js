7360f4c4e7d16a0ed94f09cff173cc77e124b017
Marie Tarantova <marie.tarantova@gmail.com>
2021-03-23 20:37:47 +0100

    Hotová kostka i se změnou obrázků

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // tady je místo pro náš program
+
+function hodKostkou(){
+
+    let cislo
+    let zprava = document.querySelector("#zprava");
+    let obrazek = document.querySelector("#kostka");
+
+    cislo = Math.floor(Math.random() *6 + 1);
+
+    if (cislo === 6){
+            zprava.innerHTML = ("Hodila jsi 6!!! Vyhráváš!");
+        } else{
+            zprava.innerHTML = ("Hodila jsi "+ cislo +"! Pokračuj ve hře!");
+    }
+
+    obrazek.src = 'obrazky/'+cislo+'.png';
+    
+}
+
+
+
+
+
